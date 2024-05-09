caddbec7c536d75efb912becc97755c4e4fd74fc
Victor San Kho Lin <victor@sankholin.com>
2024-05-09 14:15:18 +1000

    Added Docker build and push

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -1,3 +1,5 @@
+import os
+
 from constructs import Construct
 from aws_cdk import (
     Stack,
@@ -6,6 +8,7 @@ from aws_cdk import (
     aws_codepipeline as codepipeline,
     aws_codebuild as codebuild,
     aws_codepipeline_actions as codepipeline_actions,
+    aws_iam as iam,
 )
 
 
@@ -90,4 +93,63 @@ class PipelineCdkStack(Stack):
         pipeline.add_stage(
             stage_name='Code-Quality-Testing',
             actions=[build_action]
+        )
+
+        # add docker build and push artifact stage
+
+        docker_build_project = codebuild.PipelineProject(
+            self, 'Docker Build',
+            build_spec=codebuild.BuildSpec.from_source_filename('./buildspec_docker.yml'),
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged=True,
+                compute_type=codebuild.ComputeType.LARGE,
+                environment_variables={
+                    'IMAGE_TAG': codebuild.BuildEnvironmentVariable(
+                        type=codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value='latest'
+                    ),
+                    'IMAGE_REPO_URI': codebuild.BuildEnvironmentVariable(
+                        type=codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value=ecr_repository.repository_uri
+                    ),
+                    'AWS_DEFAULT_REGION': codebuild.BuildEnvironmentVariable(
+                        type=codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value=os.environ['CDK_DEFAULT_REGION']
+                    )
+                }
+            ),
+        )
+
+        docker_build_project.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=[
+                'ecr:GetAuthorizationToken',
+                'ecr:BatchCheckLayerAvailability',
+                'ecr:GetDownloadUrlForLayer',
+                'ecr:GetRepositoryPolicy',
+                'ecr:DescribeRepositories',
+                'ecr:ListImages',
+                'ecr:DescribeImages',
+                'ecr:BatchGetImage',
+                'ecr:InitiateLayerUpload',
+                'ecr:UploadLayerPart',
+                'ecr:CompleteLayerUpload',
+                'ecr:PutImage'
+            ],
+            resources=['*'],
+        ))
+
+        docker_build_output = codepipeline.Artifact()
+
+        docker_build_action = codepipeline_actions.CodeBuildAction(
+            action_name='Docker-Build',
+            project=docker_build_project,
+            input=source_output,
+            outputs=[docker_build_output]
+        )
+
+        pipeline.add_stage(
+            stage_name='Docker-Push-ECR',
+            actions=[docker_build_action]
         )
